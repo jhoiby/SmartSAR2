6f75e247efbc16a5617ff2eba0e7001648e4d64a
James Hoiby <james@hoiby.com>
2018-12-16 12:06:38 -0700

    SS-9: Add CreatePersonCommand.

---
+++ Contexts.Membership.Application/Commands/Persons/CreatePersonCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contexts.Membership.Application.Commands.Persons
+{
+    public class CreatePersonCommand
+    {
+        public CreatePersonCommand(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+    }
+}
