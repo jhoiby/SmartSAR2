5d06c4be515ac72fad82d76b01ec8c80a2e59312
James Hoiby <james@hoiby.com>
2018-12-16 12:21:38 -0700

    Add PageModelBase.

---
+++ SmartSAR.Presentation.WebUI/Pages/PageModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation.WebUI.Pages
+{
+    public class PageModelBase : PageModel
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+    }
+}
