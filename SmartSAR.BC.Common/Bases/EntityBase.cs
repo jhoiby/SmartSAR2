a965a4cbe4acfeaf440e4a38eed44ef7ff11b9c7
James Hoiby <james@hoiby.com>
2018-12-16 10:22:52 -0700

    SS-15: Add entity base class

---
+++ SmartSAR.BC.Common/Bases/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// For an excellent article on DDD base classes see
+// https://github.com/dotnet/docs/blob/master/docs/standard/microservices-architecture/microservice-ddd-cqrs-patterns/seedwork-domain-model-base-classes-interfaces.md
+// Much of this class is based on the examples there.
+
+namespace SmartSAR.BC.Common.Bases
+{
+    public abstract class EntityBase
+    {
+        private Guid _id;
+
+        public Guid Id
+        {
+            get { return _id; }
+            protected set { _id = value; }
+        }
+    }
+}
---
+++ SmartSAR.BC.Common/Interfaces/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartSAR.BC.Common.Interfaces
+{
+    public interface IEntity
+    {
+        Guid Id { get; }
+    }
+}
